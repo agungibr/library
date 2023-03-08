20829864e1491b6822a23b5f00099f77c87f4907
agungibr <agungibr@outlook.co.id>
2023-03-08 14:12:33 +0700

    -tampilan

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,17 @@
+<div class="card my-2 w-25">
+    <div class="card-img-top h-100 w-100" style="display: flex; justify-content: center; align-items: center; background-color: gray;">
+        <img src="{{ $image }}" class="w-100 align-middle" alt="sampul-buku">
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">{{ $title }}</h5>
+        <div class="d-flex w-100 justify-content-between mt-4">
+            <a href="{{ route('home.edit', $id) }}" class="btn btn-primary" style="background: #364FF6;">Ubah</a>
+            <form class="d-inline" action="{{ route('home.destroy', $id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <a href="" style="width: 140px;" class="btn btn-outline-danger confirm-delete">Hapus</a>
+            </form>
+        </div>
+        <a href="{{ route('home.show', $id) }}" class="btn btn-success w-100 mt-2 rounded-pill">Lihat</a>
+    </div>
+</div>
